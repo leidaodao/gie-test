479f351abfc186f775b6b7c7443c34f379c30808
leidaojun <1148166172@qq.com>
2019-06-08 20:07:47 +0800

    把本地代码加到git工程

---
+++ gir_day01/src/cn/itcast/Test01.java
@@ -0,0 +1,10 @@
+package cn.itcast;
+
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("你真帅");
+        System.out.println("你真美");
+        System.out.println("你真牛皮");
+        System.out.println("你真有钱");
+    }
+}
